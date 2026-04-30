80c95761ee4e92b6dd9292a613f7d78a57caae64
fabio-ivona <fabio.ivona@defstudio.it>
2026-04-30 18:41:39 +0200

    wip

--- src/Dto/TemplateMessage.php
+++ src/Dto/TemplateMessage.php
@@ -31,7 +31,13 @@ class TemplateMessage implements WhatsappMessage
 
         foreach ($components as $component) {
             if (($component['type'] ?? null) === 'BODY') {
-                return (string) ($component['text'] ?? '');
+                $text =  (string) ($component['text'] ?? '');
+
+                foreach ($this->bodyParameters as $index =>$parameter) {
+                    $text = str_replace('{{'.$index.'}}', $parameter, $text);
+                }
+
+                return $text;
             }
         }
 
